c2eb7f3ce8bad9a54cfbaba1a072ade6415c57e0
laracrofft <lara-on@yandex.ru>
2021-08-03 13:40:45 +0300

    homework added

---
+++ L9_task3_1.sql
@@ -0,0 +1,31 @@
+﻿/*
+ * Создайте хранимую функцию hello(), которая будет возвращать приветствие, в зависимости от
+текущего времени суток. С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", с
+12:00 до 18:00 функция должна возвращать фразу "Добрый день", с 18:00 до 00:00 — "Добрый
+вечер", с 00:00 до 6:00 — "Доброй ночи"
+ */
+
+DROP FUNCTION IF EXISTS func_hello;
+
+DELIMITER //
+
+CREATE FUNCTION func_hello()
+RETURNS TINYTEXT READS SQL DATA
+
+BEGIN
+	
+	DECLARE hours INT;
+	SET hours = HOUR(NOW());
+
+	CASE
+		WHEN hours BETWEEN 0 AND 5 THEN RETURN 'Доброй ночи!';
+		WHEN hours BETWEEN 6 AND 11 THEN RETURN 'Доброе утро!';
+		WHEN hours BETWEEN 12 AND 17 THEN RETURN 'Добрый день!';
+		WHEN hours BETWEEN 18 AND 23 THEN RETURN 'Добрый вечер!';
+	END CASE;
+
+END//
+
+DELIMITER ;
+
+SELECT curtime(), func_hello();
