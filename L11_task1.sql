dfc6a782f13b490cddbf92fb26e90a834fb429e7
laracrofft <lara-on@yandex.ru>
2021-08-12 11:35:53 +0300

    homework added

---
+++ L11_task1.sql
@@ -0,0 +1,64 @@
+﻿/*
+ * Создайте таблицу logs типа Archive. Пусть при каждом создании записи в таблицах users, catalogs и products
+ * в таблицу logs помещается время и дата создания записи, название таблицы, идентификатор первичного ключа
+ * и содержимое поля name.
+ */
+
+DROP TABLE IF EXISTS logs;
+
+CREATE TABLE logs (
+			created_at datetime NOT NULL,
+			table_name varchar(55) NOT NULL,
+			value_id int NOT NULL,
+			value_name varchar(255) NOT NULL
+) comment = 'Логи создания новых записей' engine=archive;
+
+DROP TRIGGER IF EXISTS check_users_log;
+
+delimiter //
+
+CREATE TRIGGER check_users_log AFTER INSERT ON users
+FOR EACH ROW
+	BEGIN 
+		INSERT INTO logs (created_at, table_name, value_id, value_name)
+		VALUES (now(), 'users', NEW.id, NEW.name);
+	END //
+
+delimiter ;
+
+DROP TRIGGER IF EXISTS check_catalogs_log;
+
+delimiter //
+
+CREATE TRIGGER check_catalogs_log AFTER INSERT ON catalogs
+FOR EACH ROW
+	BEGIN 
+		INSERT INTO logs (created_at, table_name, value_id, value_name)
+		VALUES (now(), 'catalogs', NEW.id, NEW.name);
+	END //
+
+delimiter ;
+
+DROP TRIGGER IF EXISTS check_products_log;
+
+delimiter //
+
+CREATE TRIGGER check_products_log AFTER INSERT ON products
+FOR EACH ROW
+	BEGIN 
+		INSERT INTO logs (created_at, table_name, value_id, value_name)
+		VALUES (now(), 'products', NEW.id, NEW.name);
+	END //
+
+delimiter ;
+
+INSERT INTO users (name, birthday_at)
+VALUES ('Кирилл', '1990-01-02');
+
+INSERT INTO catalogs (name)
+VALUES ('Клавиатуры');
+
+INSERT INTO products (name, description, price, catalog_id)
+VALUES ('logitech g-20', 'Клава', 5000, 6);
+
+SELECT * FROM logs;
---
+++ L11_task2.sql
@@ -0,0 +1,21 @@
+﻿-- Создайте SQL-запрос, который помещает в таблицу users миллион записей
+
+DROP PROCEDURE IF EXISTS sp_while_1000000_users;
+
+delimiter //
+
+CREATE PROCEDURE sp_while_1000000_users()
+BEGIN
+	DECLARE i int DEFAULT 1;
+	WHILE i <= 10 DO -- подставить 1000000 вместо 10
+		INSERT INTO users (name, birthday_at)
+		VALUES (concat('test_user', i), curdate());
+		SET i = i + 1;
+	END WHILE;
+END //
+
+delimiter ;
+
+CALL sp_while_1000000_users();
+
+SELECT * FROM users;
