6fcb9b73cf59b9aff55cd2dc37f2518c6f8de3a1
laracrofft <lara-on@yandex.ru>
2021-08-14 17:27:28 +0300

    course updated

--- fitness_db.sql
+++ fitness_db.sql
@@ -90,7 +90,7 @@ CREATE TABLE group_activity(
 	id SERIAL PRIMARY KEY,
 	coach_sport_type_id BIGINT UNSIGNED NOT NULL,
 	is_group_activity BOOLEAN DEFAULT FALSE,
-	member_quantity INT UNSIGNED DEFAULT 1,
+	member_quantity INT DEFAULT 1,
 	KEY (coach_sport_type_id),
 	FOREIGN KEY (coach_sport_type_id) REFERENCES coach_sport_type (id)
 );
@@ -110,7 +110,7 @@ CREATE TABLE group_members(
 
 CREATE TABLE activity_status(
 	id SERIAL PRIMARY KEY,
-	name ENUM('по плану', 'отменено', 'не назначено') NOT NULL
+	name VARCHAR(50) NOT NULL UNIQUE
 );
 
 -- "Расписание" хранит пересечение представленных выше таблиц
@@ -125,91 +125,3 @@ CREATE TABLE schedule(
 	CONSTRAINT fk_calendar_id FOREIGN KEY (calendar_id) REFERENCES calendar (id),
 	CONSTRAINT fk_activity_status_id FOREIGN KEY (activity_status_id) REFERENCES activity_status (id)
 );
-
--- также можно добавить админку для ресепшена
--- и блок авторизации
-
--- выбираем занятие группы в определенный день недели
-
-SELECT s.id, c.week_day, c.start_time, ga.coach_sport_type_id, ga.id, as2.name 
-FROM schedule s 
-	JOIN group_activity ga ON s.group_activity_id = ga.id 
-	JOIN calendar c ON s.calendar_id = c.id
-	JOIN activity_status as2 ON s.activity_status_id = as2.id 
-WHERE c.week_day = '1'
-	AND ga.id = 'some';
-
--- выбираем расписание для определенного клиента
-
-SELECT s.id, c.week_day, c.start_time, ga.coach_sport_type_id, ga.id, as2.name, gm.member_card_id
-FROM schedule s 
-	JOIN group_activity ga ON s.group_activity_id = ga.id 
-	JOIN calendar c ON s.calendar_id = c.id
-	JOIN activity_status as2 ON s.activity_status_id = as2.id
-	JOIN group_members gm ON s.group_activity_id  = gm.group_activity_id
-WHERE gm.member_card_id = 'some'
-	AND as2.name = 'по плану';
-
--- выбираем расписание для вида спорта/тренера
--- смотрим загруженность зала/зоны
-
--- проверяем наличие мест в группе
-
-DROP TRIGGER IF EXISTS check_member_quantity_before_insert;
-
-delimiter //
-
-CREATE TRIGGER check_member_quantity_before_insert BEFORE INSERT ON group_members
-FOR EACH ROW
-	BEGIN
-		IF group_activity.member_quantity = 0 THEN 
-			SIGNAL SQLSTATE '45000' SET message_text = 'места в группе закончились';
-		END IF;
-	END //
-
-delimiter ;
-
--- записываем в группу, уменьшая кол-во свободных мест (тут я наверное глупость написала и так нельзя или можно?)
-
-delimiter //
-
-CREATE PROCEDURE sp_add_member_to_group (member_card_id int, group_activity_id int)
-BEGIN
-	START TRANSACTION;
-		INSERT INTO group_members (member_card_id, group_activity_id);
-		UPDATE group_activity SET member_quantity = member_quantity - 1 WHERE group_members.group_activity_id = group_activity.id;
-	COMMIT;
-END //
-
-delimiter ;
-
--- проверка корректности ввода номера телефона
--- проверка корректности ввода номера членской карты
-
-
-
-/*
- * Вопросы:
- * - можно ли для таблицы coach_sport_type, где содержатся только пара id, использовать движок archive?
- * - а для справочников - MyISAM?
- * - имеет ли смысл вместо таблицы activity_status добавить одноименное поле в таблицу schedule?
- * - может вместо селектов использовать большое представление? не совсем поняла, что оптимальнее для скорости обработки данных
- */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
